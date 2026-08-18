2b3cc425510241c66226f6dcba239e4c7ecf8ae5
Alexis Tercero <alexistercero55@gmail.com>
2026-08-18 13:21:20 -0600

    CHANGE | #38 LogoutUseCase.kt DI setup

--- core/domain/src/main/kotlin/com/rho/studio/ui/core/domain/usecase/LogoutUseCase.kt
+++ core/domain/src/main/kotlin/com/rho/studio/ui/core/domain/usecase/LogoutUseCase.kt
@@ -10,7 +10,7 @@
  * File:         LogoutUseCase.kt
  * Author:       Alexis Tercero
  * Email:        alexis.tercero@rho.studio
- * Date:         2026-08-06
+ * Date:         2026-08-18
  * ==============================================================================================
  * Description: Handles the termination of the user session by clearing authentication tokens,
  *              resetting global application state, and ensuring secure cleanup of
@@ -19,8 +19,10 @@
  */
 package com.rho.studio.ui.core.domain.usecase
 
+import javax.inject.Inject
+
 /*** Encapsulates the logout business transaction.*/
-class LogoutUseCase(
+class LogoutUseCase @Inject constructor(
     private val sessionManager: SessionManagerInterface
 ) : BaseUseCase<Unit, Unit>() {
 
